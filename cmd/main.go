ea03e558747795853c570db96f76ad2963aff590
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-12-04 22:50:02 +0300

    feat: add gorm db

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"demo/go-server/configs"
 	"demo/go-server/internal/auth"
 	"demo/go-server/internal/hello"
+	"demo/go-server/pkg/db"
 	"fmt"
 	"net/http"
 )
@@ -13,6 +14,7 @@ func main() {
 	address := fmt.Sprintf(":%s", PORT)
 
 	conf := configs.LoadConfig()
+	_ = db.NewDb(conf)
 	router := http.NewServeMux()
 	hello.NewHelloHandler(router)
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
