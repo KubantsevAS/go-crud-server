d743235f13aa273d618871c07d282b51d674eeba
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-12-14 21:08:08 +0300

    test: add test db initialization

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -3,14 +3,46 @@ package main
 import (
 	"bytes"
 	"demo/go-server/internal/auth"
+	"demo/go-server/internal/user"
 	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+func initDb() *gorm.DB {
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func initData(db *gorm.DB) {
+	db.Create(&user.User{
+		Email:    "a@a.com",
+		Password: "$2a$10$.DuLxeEK7oFAWYt6pXmdzucWnNUDl5I2h1qP0QavAHz4Ur/bmiLZ.",
+		Name:     "John Doe",
+	})
+}
+
 func TestLoginSuccess(t *testing.T) {
+	// Precondition
+	db := initDb()
+	initData(db)
+
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
